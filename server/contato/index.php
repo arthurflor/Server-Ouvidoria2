e4ffbe5bfc63cf9d99c1bf0d29fce7e0ed0a4098
iagorichard <iagorrs@gmail.com>
2016-03-14 18:26:26 -0300

    Finalizando por hoje

--- server/contato/index.php
+++ server/contato/index.php
@@ -12,49 +12,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <!-- Esse style abaixo é para mudar a cor de alguns componentes, vem por padrão -->
+        <link rel="stylesheet" href="../css/newStyle.css">
+        
         <script src="../js/jquery.min.js"></script>
         <script src="../js/bootstrap.min.js"></script>
-
-        <!-- Esse style abaixo é para a barra de menu -->
-        <style>
-            /* Remove the navbar's default margin-bottom and rounded borders */ 
-            .navbar {
-                margin-bottom: 0;
-                border-radius: 0;
-            }
-
-            /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-            .row.content {height: 450px}
-
-            /* Set gray background color and 100% height */
-            .sidenav {
-                padding-top: 20px;
-                background-color: #f1f1f1;
-                height: 100%;
-            }
-
-            /* Set black background color, white text and some padding */
-            footer {
-                background-color: #555;
-                color: white;
-                padding: 15px;
-            }
-
-            /* On small screens, set height to 'auto' for sidenav and grid */
-            @media screen and (max-width: 767px) {
-                .sidenav {
-                    height: auto;
-                    padding: 15px;
-                }
-                .row.content {height:auto;} 
-            }
-        </style>
     </head>
     <body>
         <!-- logo -->
         <div class="row">
-            <div class="container-fluid">
-                <img src="../images/logo.png"/>
+            <div class="container-fluid"><a href="../">
+                    <img src="../images/logo.png" alt="logo"/>
+                </a>
             </div>
         </div>
         
--- server/relatorios/index.php
+++ server/relatorios/index.php
@@ -11,49 +11,19 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <!-- Esse style abaixo é para mudar a cor de alguns componentes, vem por padrão -->
+        <link rel="stylesheet" href="../css/newStyle.css">
+        
         <script src="../js/jquery.min.js"></script>
         <script src="../js/bootstrap.min.js"></script>
-
-        <!-- Esse style abaixo é para a barra de menu -->
-        <style>
-            /* Remove the navbar's default margin-bottom and rounded borders */ 
-            .navbar {
-                margin-bottom: 0;
-                border-radius: 0;
-            }
-
-            /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-            .row.content {height: 450px}
-
-            /* Set gray background color and 100% height */
-            .sidenav {
-                padding-top: 20px;
-                background-color: #f1f1f1;
-                height: 100%;
-            }
-
-            /* Set black background color, white text and some padding */
-            footer {
-                background-color: #555;
-                color: white;
-                padding: 15px;
-            }
-
-            /* On small screens, set height to 'auto' for sidenav and grid */
-            @media screen and (max-width: 767px) {
-                .sidenav {
-                    height: auto;
-                    padding: 15px;
-                }
-                .row.content {height:auto;} 
-            }
-        </style>
     </head>
     <body>
         <!-- logo -->
         <div class="row">
             <div class="container-fluid">
-                <img src="../images/logo.png"/>
+                <a href="../">
+                    <img src="../images/logo.png" alt="logo"/>
+                </a>
             </div>
         </div>
         
@@ -89,7 +59,7 @@
             </div>
         </nav>
 
-        <div class="container-fluid text-center">    
+        <div class="container-fluid text-center" id="id_da_div">    
             <div class="row content">
                 
                 <!-- barra da esquerda -->
@@ -131,7 +101,19 @@
                 </div>
             </div>
         </div>
+        
+        <!-- Esse script manda o conteúdo de uma div para impressão-->
+        <script>
+            document.getElementById('id_do_button').onclick = function() {
+                var conteudo = document.getElementById('id_da_div').innerHTML,
+                    tela_impressao = window.open('about:blank');
 
+                tela_impressao.document.write(conteudo);
+                tela_impressao.window.print();
+                tela_impressao.window.close();
+            };
+        </script>
+        
         <!-- fim da página -->
         <footer class="container-fluid text-center">
             <p>Prefeitura Municipal de Caruaru | Todos os Direitos Reservados | Desenvolvido por Universidade de Pernambuco (FACITEC)</p>
